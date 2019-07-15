5779c85a07c409c4c2f0f70648bd7b0295893470
Justin <justinyang1000@hotmail.com>
2019-07-15 09:31:55 -0400

    Initial commit

---
+++ src/main/java/com/jy/bootstrapbuttons/BootstrapbuttonsApplication.java
@@ -0,0 +1,13 @@
+package com.jy.bootstrapbuttons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapbuttonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapbuttonsApplication.class, args);
+    }
+
+}
